75203450f54dcfc5d2d8b357f79bfe7bb321fe4b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-04-19 13:01:07 -0500

    REPL From FEM Nina's class

---
+++ pyworkshop/1_intro_python/chapter1/REPL.py
@@ -0,0 +1,20 @@
+#REPL
+Read, Evaluate, Print, Loop
+name="g3"
+# type(name)
+# dir(name)
+# name.upper()
+# help(str) -> pass the type
+
+# help(str.upper)
+
+# type, help and dir are best friends
+
+############
+
+# COMMON REPL MISTAKES
+
+# Variables cannot start with a number
+# Read trace backs from bottom to top
+# Don't user reserved words for variable names
+# 
